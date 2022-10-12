eaa3e4c34d55fa014b4c6404b67ff366e3b79a1b
Vishal Srivastava <eth.vishal@gmail.com>
2022-10-12 13:32:58 +0530

    Modified App.css and added favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,123 +1,100 @@
-import Header from "./components/Header"
-import Meme from "./components/Meme"
-import "./App.css"
-import { useEffect, useState } from "react"
-
+import Header from "./components/Header";
+import Meme from "./components/Meme";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 const App = () => {
+  console.log("hello");
 
-  console.log("hello")
+  const [memeData, setMemeData] = useState([]);
 
-  const [memeData, setMemeData] = useState([])
-  
   const [meme, setMeme] = useState({
     topText: "",
     bottomText: "",
-    imageUrl: ""
-  })
-
+    imageUrl: "",
+  });
 
   const handleChange = (event) => {
-    console.log(event.target)
+    console.log(event.target);
     const { value, name } = event.target;
 
     setMeme((preState) => {
-      return (
-        {
-          ...preState,
-          [name]: value
-        }
-      )
-    })
-  }
+      return {
+        ...preState,
+        [name]: value,
+      };
+    });
+  };
 
+  useEffect(() => {
+    console.log("useEffect called");
+    console.log("hello");
 
-    useEffect(() => {
-      console.log("useEffect called");
-      console.log("hello")
+    fetch("https://api.imgflip.com/get_memes")
+      .then((res) => res.json())
+      .then((data) => setMemeData(data));
+  }, []);
 
-      fetch("https://api.imgflip.com/get_memes")
-        .then(res => res.json())
-        .then(data => setMemeData(data))
+  const getUrl = () => {
+    const imageArray = memeData.data.memes;
+    console.log(imageArray[0].url, "hi");
 
-        
-     
-    }, [])
+    const randomIndex = Math.floor(Math.random() * imageArray.length);
 
+    setMeme((preState) => {
+      return {
+        ...preState,
+        imageUrl: imageArray[randomIndex].url,
+      };
+    });
+  };
 
-    const getUrl = () => {
-      
-      const imageArray = memeData.data.memes;
-      console.log(imageArray[0].url, "hi")
-      
-      
-      const randomIndex = Math.floor(Math.random() * imageArray.length)
-      
-      setMeme((preState) => {
-        return (
-          {
-            ...preState,
-            imageUrl: imageArray[randomIndex].url
-          }
-        )
-      })
-      
-    }
-
-
-
-
-    return (
-      <div className="container">
-
-            <div className="item1">
-              <Header />
-            </div>
-
-                  
-              <div className="item2">
-                <input
-                  type="text"
-                  className="textBox"
-                  name="topText"
-                  onChange={handleChange}
-                  value={meme.topText}
-                  id=""
-                  placeholder="Top Text"
-                />
-              </div>
-        
-        
-              <div className="item3">
-                <input
-                  type="text"
-                  className="textBox"
-                  name="bottomText"
-                  onChange={handleChange}
-                  value={meme.bottomText}
-                  id=""
-                  placeholder= "Bottom Text"
-                />
-              </div>
-
-
-            <div className="memeSection">
-              <input className="btn web3color" type="button" onClick={getUrl} value="Generate Meme" />
-
-             
-
-                <Meme url={meme.imageUrl} topText = {meme.topText} bottomText = {meme.bottomText} />
-              
-              </div>
-
-        
-    
-
-
+  return (
+    <div className="container">
+      <div className="item1">
+        <Header />
       </div>
-    )
-  
-}
 
+      <div className="item2">
+        <input
+          type="text"
+          className="textBox"
+          name="topText"
+          onChange={handleChange}
+          value={meme.topText}
+          id=""
+          placeholder="Top Text"
+        />
+      </div>
+
+      <div className="item3">
+        <input
+          type="text"
+          className="textBox"
+          name="bottomText"
+          onChange={handleChange}
+          value={meme.bottomText}
+          id=""
+          placeholder="Bottom Text"
+        />
+      </div>
+
+      <div className="memeSection">
+        <input
+          className="btn web3color"
+          type="button"
+          onClick={getUrl}
+          value="Generate Meme"
+        />
+
+        <Meme
+          url={meme.imageUrl}
+          topText={meme.topText}
+          bottomText={meme.bottomText}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default App;
--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,14 +1,11 @@
 const Meme = (props) => {
-        
-    return (
-        <div className="memeComponent">
-            {props.url && <img src={props.url} alt="" className="memeImage" />}|
-            
-                <p className="memeText top">{props.topText}</p>
-                <p className="memeText bottom">{props.bottomText}</p>
-        </div>
-        
-    )
-}
+  return (
+    <div className="memeComponent">
+      {props.url && <img src={props.url} alt="" className="memeImage" />}
+      <p className="memeText top">{props.topText}</p>
+      <p className="memeText bottom">{props.bottomText}</p>
+    </div>
+  );
+};
 
 export default Meme;
